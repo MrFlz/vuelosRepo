ecd6adff589f8056e656820cf66aa0124a92244b
MrFlz <flonoalemil@gmail.com>
2019-12-03 18:11:57 -0500

    cuarto commit

---
+++ reservar/reservar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservar',
+  templateUrl: './reservar.component.html',
+})
+export class ReservarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
